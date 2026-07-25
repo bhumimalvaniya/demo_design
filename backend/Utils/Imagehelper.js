af3b157c5d10dd5dd608df83f468f4360d506d47
Bhumi Malvaniya <bhumimalvaniya@gmail.com>
2026-07-25 11:46:33 +0530

    add of imagehelper page

--- backend/Utils/Imagehelper.js
+++
@@ -1,9 +0,0 @@
-
-import API_URL from "../config/api";
-
-export const getImageUrl = (img) => {
-  if (!img) return "/placeholder.jpg";
-  if (img.startsWith("http://") || img.startsWith("https://")) return img;
-  if (img.startsWith("/public")) return `${API_URL}/${img.replace("/public", "")}`;
-  return `${API_URL}/${img.replace(/^\/+/, "")}`;
-};
